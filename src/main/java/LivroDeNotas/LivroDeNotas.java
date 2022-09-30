ab0283bdca3f998ed3f5fecbc68f6d6c5a484949
Danilo-Miguel <danilo.miguel@cromai.com>
2022-09-30 20:27:22 -0300

    Feat: Adição do cadastro e exibição das notas do aluno

--- src/main/java/LivroDeNotas/LivroDeNotas.java
+++ src/main/java/LivroDeNotas/LivroDeNotas.java
@@ -1,18 +1,35 @@
+package LivroDeNotas;
+
 import javax.swing.JOptionPane;
 
+
 public class LivroDeNotas {
-    
+
     private String nomeDoCurso;
     private String nomeProfessor;
-    
-   public LivroDeNotas(String nomeDoCurso, String nomeProfessor){
-       this.nomeDoCurso = nomeDoCurso;
-       this.nomeProfessor = nomeProfessor;  
-   }
-  
-    public void exibirMensagem(){
+    private String nomeAluno;
+
+    private double nota1, nota2, nota3, notaFinal;
+
+    public LivroDeNotas(String nomeDoCurso, String nomeProfessor, String nomeAluno) {
+        this.nomeDoCurso = nomeDoCurso;
+        this.nomeProfessor = nomeProfessor;
+        this.nomeAluno = nomeAluno;
+    }
+
+    public void exibirMensagem() {
         JOptionPane.showMessageDialog(null, "Bem vindo Professor " + getNomeProfessor() + " ao livro de notas do curso " + nomeDoCurso);
-    } 
+    }
+
+    public void cadastrarNota() {
+        setNotaFinal(getNota1() + getNota2() + getNota3());
+        JOptionPane.showMessageDialog(null, "Nota cadastrada com sucesso!");
+
+    }
+
+    public void exibirNota() {
+        JOptionPane.showMessageDialog(null, " A nota do aluno é: " + getNotaFinal());
+    }
 
     public String getNomeDoCurso() {
         return nomeDoCurso;
@@ -35,5 +52,75 @@ public class LivroDeNotas {
     public void setNomeProfessor(String nomeProfessor) {
         this.nomeProfessor = nomeProfessor;
     }
-      
+
+    /**
+     * @return the nomeAluno
+     */
+    public String getNomeAluno() {
+        return nomeAluno;
+    }
+
+    /**
+     * @param nomeAluno the nomeAluno to set
+     */
+    public void setNomeAluno(String nomeAluno) {
+        this.nomeAluno = nomeAluno;
+    }
+
+    /**
+     * @return the nota1
+     */
+    public double getNota1() {
+        return nota1;
+    }
+
+    /**
+     * @param nota1 the nota1 to set
+     */
+    public void setNota1(double nota1) {
+        this.nota1 = nota1;
+    }
+
+    /**
+     * @return the nota2
+     */
+    public double getNota2() {
+        return nota2;
+    }
+
+    /**
+     * @param nota2 the nota2 to set
+     */
+    public void setNota2(double nota2) {
+        this.nota2 = nota2;
+    }
+
+    /**
+     * @return the nota3
+     */
+    public double getNota3() {
+        return nota3;
+    }
+
+    /**
+     * @param nota3 the nota3 to set
+     */
+    public void setNota3(double nota3) {
+        this.nota3 = nota3;
+    }
+
+    /**
+     * @return the notaFinal
+     */
+    public double getNotaFinal() {
+        return notaFinal;
+    }
+
+    /**
+     * @param notaFinal the notaFinal to set
+     */
+    public void setNotaFinal(double notaFinal) {
+        this.notaFinal = notaFinal;
+    }
+
 }
--- src/main/java/LivroDeNotas/TesteLivroDeNotas.java
+++ src/main/java/LivroDeNotas/TesteLivroDeNotas.java
@@ -1,18 +1,32 @@
+package LivroDeNotas;
 
 import javax.swing.JOptionPane;
 
 public class TesteLivroDeNotas {
 
     public static void main(String[] args) {
+
         String nomeDoCurso = JOptionPane.showInputDialog("Qual o nome do curso Professor?");
         String nomeProfessor = JOptionPane.showInputDialog("Qual o nome do Professor?");
-        String nomeDoCurso1 = JOptionPane.showInputDialog("Qual o nome do curso Professor?");
-        String nomeProfessor1 = JOptionPane.showInputDialog("Qual o nome do Professor?");
+        String aluno = JOptionPane.showInputDialog("Qual o nome do Aluno?");
 
-        LivroDeNotas livroDeNotas = new LivroDeNotas(nomeDoCurso, nomeProfessor);
-        LivroDeNotas livroDeNotas1 = new LivroDeNotas(nomeDoCurso1, nomeProfessor1);
+        LivroDeNotas livroDeNotas = new LivroDeNotas(nomeDoCurso, nomeProfessor, aluno);
 
         livroDeNotas.exibirMensagem();
+
+        //String nomeDoCurso1 = JOptionPane.showInputDialog("Qual o nome do curso Professor?");
+        // String nomeProfessor1 = JOptionPane.showInputDialog("Qual o nome do Professor?");
+        double nota1 = Double.parseDouble(JOptionPane.showInputDialog("Digite a nota da A1"));
+        double nota2 = Double.parseDouble(JOptionPane.showInputDialog("igite a nota da A2"));
+        double nota3 = Double.parseDouble(JOptionPane.showInputDialog("igite a nota da A3"));
+
+        //LivroDeNotas livroDeNotas1 = new LivroDeNotas(nomeDoCurso1, nomeProfessor1);
+        livroDeNotas.setNota1(nota1);
+        livroDeNotas.setNota2(nota2);
+        livroDeNotas.setNota3(nota3);
+
+        livroDeNotas.cadastrarNota();
+        livroDeNotas.exibirNota();
 
     }
 
